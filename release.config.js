ad06a11b94c6f701b68bcbf94e0b97181b52b31c
Lawrence <lawrence@acode.ninja>
2024-09-09 11:51:32 +0100

    fix: use conventional commits for releases

--- release.config.js
+++ release.config.js
@@ -4,8 +4,18 @@
 export default {
     branches: ['main'],
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+              preset: 'conventionalcommits',
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+              preset: 'conventionalcommits',
+            },
+        ],
         '@semantic-release/github',
         '@semantic-release/npm',
     ],
