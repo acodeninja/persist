eaf835953de5c2dd78054e0b118d83ecf02795f7
Lawrence <lawrence@acode.ninja>
2025-01-30 07:24:54 +0000

    test: cover creating a new storage engine

--- src/engine/StorageEngine.js
+++ src/engine/StorageEngine.js
@@ -3,7 +3,7 @@ export default class StorageEngine {
      * @param {Object} configuration
      * @param {Array<Model>?} models
      */
-    constructor(configuration, models = null) {
+    constructor(configuration = {}, models = null) {
         this.configuration = configuration;
         this.models = Object.fromEntries((models ?? []).map(model => [model.name, model]));
     }
