e65b909476515f0a3bb69dbb869afe349a1e4880
Lawrence <lawrence@acode.ninja>
2025-02-26 08:43:50 +0000

    refactor: move test storage engine to fixtures

---
+++ test/fixtures/Engine.js
@@ -0,0 +1,13 @@
+import StorageEngine, {ModelNotFoundStorageEngineError} from '../../src/engine/StorageEngine.js';
+import {jest} from '@jest/globals';
+
+export class TestStorageEngine extends StorageEngine {
+    constructor(configuration = {}, models = null) {
+        super(configuration, models);
+        this._deleteModel = jest.fn();
+        this._putIndex = jest.fn();
+        this._getIndex = jest.fn();
+        this._putModel = jest.fn();
+        this._getModel = jest.fn().mockImplementation((id) => Promise.reject(new ModelNotFoundStorageEngineError(id)));
+    }
+}
