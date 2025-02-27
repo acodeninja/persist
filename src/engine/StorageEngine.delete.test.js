ca69d0c0cd4a71eb6225386d64398a67d4cfdb0e
Lawrence <lawrence@acode.ninja>
2025-02-27 08:15:53 +0000

    feat: deleting a model removes it from the search index

--- src/engine/StorageEngine.delete.test.js
+++ src/engine/StorageEngine.delete.test.js
@@ -2,7 +2,10 @@ import {
     LinkedModel,
     LinkedModelFactory,
     SimpleModel,
-    SimpleModelFactory, SimpleModelWithIndex, SimpleModelWithIndexFactory,
+    SimpleModelFactory,
+    SimpleModelWithIndex,
+    SimpleModelWithIndexFactory, SimpleModelWithSearchIndex,
+    SimpleModelWithSearchIndexFactory,
 } from '../../test/fixtures/Model.js';
 import {
     ModelNotFoundStorageEngineError,
@@ -132,6 +135,70 @@ describe('StorageEngine.delete()', () => {
 
         test('._putIndex() is called with the main model removed', () => {
             expect(engine._putIndex).toHaveBeenCalledWith(model.constructor, {});
+        });
+    });
+
+    describe('when a model with a search index exists', () => {
+        const model = SimpleModelWithSearchIndexFactory();
+        const engine = new TestStorageEngine({}, [SimpleModelWithSearchIndex]);
+
+        beforeAll(async () => {
+            engine._getModel.mockResolvedValue(model);
+            engine._getIndex.mockResolvedValue({
+                [model.id]: model.toIndexData(),
+            });
+            engine._getSearchIndex.mockResolvedValue({
+                [model.id]: model.toSearchData(),
+            });
+            await engine.delete(model);
+        });
+
+        test('._getModel() is called once', () => {
+            expect(engine._getModel).toHaveBeenCalledTimes(1);
+        });
+
+        test('._getModel() is called with the main model id', () => {
+            expect(engine._getModel).toHaveBeenCalledWith(model.id);
+        });
+
+        test('._deleteModel() is called once', () => {
+            expect(engine._deleteModel).toHaveBeenCalledTimes(1);
+        });
+
+        test('._deleteModel() is called with the main model', () => {
+            expect(engine._deleteModel).toHaveBeenCalledWith(model.id);
+        });
+
+        test('._getIndex() is called once', () => {
+            expect(engine._getIndex).toHaveBeenCalledTimes(1);
+        });
+
+        test('._getIndex() is called with the main model constructor', () => {
+            expect(engine._getIndex).toHaveBeenCalledWith(model.constructor);
+        });
+
+        test('._putIndex() is called once', () => {
+            expect(engine._putIndex).toHaveBeenCalledTimes(1);
+        });
+
+        test('._putIndex() is called with the main model removed', () => {
+            expect(engine._putIndex).toHaveBeenCalledWith(model.constructor, {});
+        });
+
+        test('._getSearchIndex() is called once', () => {
+            expect(engine._getSearchIndex).toHaveBeenCalledTimes(1);
+        });
+
+        test('._getSearchIndex() is called with the main model constructor', () => {
+            expect(engine._getSearchIndex).toHaveBeenCalledWith(model.constructor);
+        });
+
+        test('._putSearchIndex() is called once', () => {
+            expect(engine._putSearchIndex).toHaveBeenCalledTimes(1);
+        });
+
+        test('._putSearchIndex() is called with the main model removed', () => {
+            expect(engine._putSearchIndex).toHaveBeenCalledWith(model.constructor, {});
         });
     });
 });
